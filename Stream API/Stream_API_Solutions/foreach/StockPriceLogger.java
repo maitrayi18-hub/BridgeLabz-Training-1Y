0fc2e6e76f5e46c35e067a093e08d18e06f245da
Ansh <anshverma151106@gmail.com>
2026-04-27 15:46:57 +0530

    Added

--- Stream API/Stream_API_Solutions/foreach/StockPriceLogger.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class StockPriceLogger {
-    public static void main(String[] args) {
-        List<Double> prices = Arrays.asList(100.5, 200.75, 150.0);
-        prices.forEach(System.out::println);
-    }
-}
--- Stream API/Stream_API_Solutions/foreach/WelcomeMessage.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class WelcomeMessage {
-    public static void main(String[] args) {
-        List<String> attendees = Arrays.asList("Ansh", "Rahul");
-        attendees.forEach(a -> System.out.println("Welcome " + a));
-    }
-}
