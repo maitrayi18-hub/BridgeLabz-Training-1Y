f3a2a4b6fb3df7f714e38a2957af667dc4292850
Ansh <anshverma151106@gmail.com>
2026-04-27 15:47:03 +0530

    Added

--- Workshop/BridgeLabz-Training-1Y-workshop/Arrays/ArrayExample.java
+++
@@ -1,9 +0,0 @@
-class ArrayExample{
-public static void main(String[] args){
-int marks[] = {10,11,14,17,19};
-System.out.println("Array elements:");
-for (int i = 0;i<marks.length; i++){
-        System.out.println(marks[i]);}//printing elements
-    System.out.println(marks[2]);//accessing elements
-    marks[3]=357;
-    }}
--- Workshop/BridgeLabz-Training-1Y-workshop/oops.java
+++
@@ -1,6 +0,0 @@
-//abstract class
-abstract class oops {
-    //Abstract method(no body)
-    abstract void calculateArea();
-    //normal method 
-}
